236a945a22ed59ef1476afb8c355e1f7839891cb
PirateCoderz <galpha964@gmail.com>
2023-08-30 16:09:30 +0500

    remove function is added

--- src/components/Carts/Cart.js
+++ src/components/Carts/Cart.js
@@ -12,8 +12,12 @@ const Cart = (props) => {
     const hasItems = cartCtx.items.length > 0;
     const orderButton = <button className={classes.button}>Order</button>;
 
-    const cartItemRemoveHandler = (id) => {};
-    const cartItemAddHandler = (item) => {};
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({ ...item, amount: 1 })
+    };
     const cartItems = <ul className={classes['cart-items']}>
         {cartCtx.items.map((item) => (
             <CartItem
